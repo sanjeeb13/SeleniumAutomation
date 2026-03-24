327ef5a47bb0f09d34a1e1492a3d63c1ee0d18cc
Sanjeeb Kumar <sanjeeb.pattnaik3@gmail.com>
2026-03-25 04:13:44 +0530

    added explicity wait

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utils.Log;
 
@@ -18,7 +20,14 @@ public class LoginPage {
 	@FindBy(id="Password")
 	WebElement passwordTextbox;
 	
-	@FindBy(xpath="//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button")
+	//@FindBy(xpath="//*[@id=\"main\"]/div/div/div/div[2]/div[1]/div/form/div[3]/button")
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+	WebElement button = wait.until(
+    ExpectedConditions.elementToBeClickable(
+        By.xpath("//*[@id='main']/div/div/div/div[2]/div[1]/div/form/div[3]/button")
+    	)
+		)	;
+button.click();
 	WebElement loginButton;
 	
 //	private By usernameTextBox = By.id("Email");
